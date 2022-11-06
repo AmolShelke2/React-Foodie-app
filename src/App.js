f0fa793ad87ee46f11bf41fcfd43d45905dd6355
Amol shelke <shelkeamol897@gmail.com>
2022-11-06 13:45:44 +0530

    Feat: Added Home section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import "./App.css";
+
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 const App = () => {
   return (
     <div className="main-container">
       <Header />
+      <Home />
     </div>
   );
 };
---
+++ src/components/home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import homeImage from "../../../src/images/home-img.png";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="content">
+        <h3>Food made with love</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex aliquam
+          suscipit nemo, natus assumenda, similique placeat illo distinctio
+          exercitationem ea, mollitia officiis nulla vel iure illum aliquid
+          eaque et accusamus?
+        </p>
+        <a href="#!" className="btn">
+          order now
+        </a>
+      </div>
+
+      <div className="image">
+        <img src={homeImage} alt="home page image" />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
