073178792077564f1306d759b1df471644dc849c
Amol shelke <shelkeamol897@gmail.com>
2022-11-08 12:29:47 +0530

    Feat: Added Onchange and onSubmit handler on order form

--- src/components/order/OrderForm.js
+++ src/components/order/OrderForm.js
@@ -6,33 +6,70 @@ const OrderForm = () => {
   const [userEmailInput, setUserEmailInput] = useState('');
   const [userFoodInput, setUserFoodInput] = useState('');
 
+  const userNameChangeHandler = e => {
+    setUserNameInput(e.target.value);
+  };
+
+  const userPhoneNumberChangeHandler = e => {
+    setUserPhoneNumber(e.target.value);
+  };
+
+  const userEmailChangeHandler = e => {
+    setUserEmailInput(e.target.value);
+  };
+
+  const userFoodChangeHandler = e => {
+    setUserFoodInput(e.target.value);
+  };
+
   const submitHandler = e => {
     e.preventDefault();
+
+    setUserNameInput('');
+    setUserEmailInput('');
+    setUserFoodInput('');
+    setUserPhoneNumber('');
   };
 
   return (
     <form onSubmit={submitHandler}>
       <div className="inputBox">
         <label>name</label>
-        <input type="text" value={userNameInput} />
+        <input
+          type="text"
+          value={userNameInput}
+          onChange={userNameChangeHandler}
+        />
       </div>
 
       <div className="inputBox">
         <label>Phone</label>
-        <input type="number" value={userPhoneNumberInput} />
+        <input
+          type="number"
+          value={userPhoneNumberInput}
+          onChange={userPhoneNumberChangeHandler}
+        />
       </div>
 
       <div className="inputBox">
         <label>Email</label>
-        <input type="text" value={userEmailInput} />
+        <input
+          type="text"
+          value={userEmailInput}
+          onChange={userEmailChangeHandler}
+        />
       </div>
 
       <div className="inputBox">
         <label>Food</label>
-        <input type="text" value={userFoodInput} />
+        <input
+          type="text"
+          value={userFoodInput}
+          onChange={userFoodChangeHandler}
+        />
       </div>
 
-      <button type="submit" className="btn">
+      <button type="submit" className="btn" onSubmit={submitHandler}>
         order now
       </button>
     </form>
